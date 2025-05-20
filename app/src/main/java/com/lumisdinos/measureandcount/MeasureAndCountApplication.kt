f8484ce48a10f162914fc31c8f8ed518e42e6f21
alexSimch <alexey@xealenergy.com>
2025-05-20 09:00:53 -0400

    Add firebase crashlytics and analytics dependencies

--- app/src/main/java/com/lumisdinos/measureandcount/MeasureAndCountApplication.kt
+++ app/src/main/java/com/lumisdinos/measureandcount/MeasureAndCountApplication.kt
@@ -2,12 +2,14 @@ package com.lumisdinos.measureandcount
 
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MeasureAndCountApplication  : Application() {
     override fun onCreate() {
         super.onCreate()
+        FirebaseApp.initializeApp(this)
         MobileAds.initialize(this) {}
     }
 }
