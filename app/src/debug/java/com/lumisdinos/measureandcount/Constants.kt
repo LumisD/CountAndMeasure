a35117624a765b71769856dc0b6bed673b4d8930
alexSimch <alexey@xealenergy.com>
2025-05-21 11:29:53 -0400

    Fix ads

---
+++ app/src/debug/java/com/lumisdinos/measureandcount/Constants.kt
@@ -0,0 +1,4 @@
+@file:JvmName("DebugConstants")
+package com.lumisdinos.measureandcount
+
+const val BANNER_AD_UNIT_ID  = "ca-app-pub-3940256099942544/6300978111"// Test ID
---
+++ app/src/release/java/com/lumisdinos/measureandcount/Constants.kt
@@ -0,0 +1,4 @@
+@file:kotlin.jvm.JvmName("ReleaseConstants")
+package com.lumisdinos.measureandcount
+
+const val BANNER_AD_UNIT_ID  = "ca-app-pub-5116102438215759/3926109002"// Release id
